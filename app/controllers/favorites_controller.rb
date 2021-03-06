93b5755d785700bad13aba2bf1c95bbc4bb6e291
gentarou.yamazaki <going3956@yahoo.co.jp>
2021-03-06 12:36:45 +0000

    いいね機能の実装まで

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  def create
+    post = Post.find(params[:post_id])
+    current_user.favorite(post)
+    flash[:success] = 'いいねしました'
+    redirect_back(fallback_location: root_url)
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    current_user.unfavorite(post)
+    flash[:success] = 'いいねを解除しました'
+    redirect_back(fallback_location: root_url)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  #いいね機能
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :post
 
   def follow(other_user)
     unless self == other_user
@@ -25,6 +29,19 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def favorite(other_post)
+    self.favorites.find_or_create_by(post_id: other_post.id)
+  end
+
+  def unfavorite(other_post)
+    favorite = self.favorites.find_by(post_id: other_post.id)
+    favorite.destroy if favorite
+  end
+
+  def favorite?(other_post)
+    self.likes.include?(other_post)
   end
   
   def feed_posts
