1758225a2836c3cc42d17291e31634178e06b27e
gentarou.yamazaki <going3956@yahoo.co.jp>
2021-03-21 06:37:24 +0000

    ユーザー画像の編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites, source: :user
 
 end
