a373a18f00b1e2ce63ce3af35e6f24b633374e1f
gentarou.yamazaki <going3956@yahoo.co.jp>
2021-04-04 09:42:40 +0000

    safe_joinの実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   
   belongs_to :user
 
-  validates :content, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 300 }
   
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites, source: :user
