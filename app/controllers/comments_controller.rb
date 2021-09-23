24726ee25fb03a43ae94ebaa6b657caf463ff3a8
gentaro.yamazaki <going3956@yahoo.co.jp>
2021-09-23 11:51:06 +0900

    コメントajex

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,8 +12,9 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      flash[:success] = 'コメントを投稿しました。'
-      redirect_back(fallback_location: root_path)
+      # flash[:success] = 'コメントを投稿しました。'
+      # redirect_back(fallback_location: root_path)
+      render :index
     else
       flash.now[:danger] = 'コメントの投稿に失敗しました。'
       redirect_back(fallback_location: root_path)
